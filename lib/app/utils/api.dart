1884a3bc68439de23dfaf4ec120fa1444430e6ac
muhammadraflyrasyidin <muhammadraflyrasyidin@gmail.com>
2025-01-16 07:43:23 +0000

    dua

--- lib/app/utils/api.dart
+++ lib/app/utils/api.dart
@@ -5,6 +5,6 @@ class BaseUrl {
 
   static String events = 'https://praujikom.smkassalaambandung.sch.id/api/events';
   static String yourEvent = 'https://praujikom.smkassalaambandung.sch.id/api/your-event';
-  static String detailEvents = 'https://praujikom.smkassalaambandung.sch.id/api/events/';
+  static String detailEvents = 'https://praujikom.smkassalaambandung.sch.id/api/events';
   static String deleteEvents = 'https://praujikom.smkassalaambandung.sch.id/api/events/';
 }
